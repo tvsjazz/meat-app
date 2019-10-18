b52c50887b115fdfded970914fc86b5f180a931d
Tiago Siqueira <tiagov@badesc.gov.br>
2019-10-18 17:49:39 -0300

    classe de tratamento de erros

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Restaurant } from './restaurant/restaurant.model';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 import { MEAT_API } from '../app.api';
 import { Observable } from 'rxjs/Observable';
+import { ErrorHandler } from 'app/app.error-handler';
 
 @Injectable()
 export class RestaurantsService {
@@ -12,6 +14,8 @@ export class RestaurantsService {
     constructor(private http: Http) {}
 
     restaurants(): Observable<Restaurant[]> {
-       return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json());
+       return this.http.get(`${MEAT_API}/restaurants`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError);
     }
 }
