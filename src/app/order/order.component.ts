e9e2f467beea4037d387cc2203dfbaff1b90dc7c
Tiago Siqueira <tvsjazz@gmail.com>
2019-10-28 10:04:44 -0300

    component total de compras

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RadioOption } from 'app/shared/radio/radio.option.model';
+import { OrderService } from './order.service';
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 
 @Component({
   selector: 'mt-order',
@@ -7,15 +9,37 @@ import { RadioOption } from 'app/shared/radio/radio.option.model';
 })
 export class OrderComponent implements OnInit {
 
+  delivery: number = 8;
+
   paymentOptions: RadioOption[] = [
     {label: 'Dinheiro', value: 'MON'},
     {label: 'Cartão de Débito', value: 'DEB'},
     {label: 'Cartão Refeição', value: 'REF'}
   ];
 
-  constructor() { }
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+  }
+
+  itemsValue(): number {
+    return this.orderService.itemsValue();
+  }
+
+  cartItems(): CartItem[] {
+    return this.orderService.cartItems();
+  }
+
+  increaseQty(item: CartItem) {
+    this.orderService.increaseQty(item);
+  }
+
+  decreaseQty(item: CartItem) {
+    this.orderService.decreaseQty(item);
+  }
+
+  remove(item: CartItem) {
+    this.orderService.remove(item);
   }
 
 }
