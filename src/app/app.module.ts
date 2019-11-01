8040065de8fd246a137bee261837c0b2e5d21d97
Tiago Vieira Siqueira <tiagov@badesc.gov.br>
2019-11-01 12:43:36 -0300

    criando shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,13 +19,10 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { OrderService } from './order/order.service';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
-import { RatingComponent } from './shared/rating/rating.component';
 
 
 @NgModule({
@@ -41,12 +38,9 @@ import { RatingComponent } from './shared/rating/rating.component';
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent,
-    RadioComponent,
     OrderItemsComponent,
     DeliveryCostsComponent,
-    OrderSumaryComponent,
-    RatingComponent
+    OrderSumaryComponent
   ],
   imports: [
     BrowserModule,
