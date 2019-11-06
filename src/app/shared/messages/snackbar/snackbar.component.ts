5053300d83e79f2d05f5c640f40cddec1ece457b
Tiago Siqueira <tiagov@badesc.gov.br>
2019-11-06 13:25:36 -0300

    terminando a animação do snackbar

--- src/app/shared/messages/snackbar/snackbar.component.ts
+++ src/app/shared/messages/snackbar/snackbar.component.ts
@@ -3,6 +3,9 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 import { NotificationService } from '../notification.service';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/timer';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/switchMap';
+import { timer } from 'rxjs/observable/timer';
 
 @Component({
   selector: 'mt-snackbar',
@@ -32,11 +35,12 @@ export class SnackbarComponent implements OnInit {
   constructor(private notificationService: NotificationService) { }
 
   ngOnInit() {
-    this.notificationService.notifier.subscribe(message => {
-      this.message = message;
-      this.snackVisibility = 'visible';
-      Observable.timer(3000).subscribe(timer => this.snackVisibility = 'hidden');
-    })
+    this.notificationService.notifier
+      .do(message => {
+        this.message = message;
+        this.snackVisibility = 'visible';
+    }).switchMap(message => Observable.timer(3000))
+      .subscribe( timer => this.snackVisibility = 'hidden')
   }
 
 }
