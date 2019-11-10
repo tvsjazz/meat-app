3554902ba7d8f460ac5bdb3f51f6f4f9388c89af
Tiago Siqueira <tvsjazz@gmail.com>
2019-11-10 10:05:30 -0300

    feito em casa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,6 @@ import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { OrderComponent } from './order/order.component';
 import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
 
 export const ROUTES: Routes = [
@@ -17,7 +16,7 @@ export const ROUTES: Routes = [
             {path: 'menu', component: MenuComponent},
             {path: 'reviews', component: ReviewsComponent}
         ]},
-    {path: 'order', component: OrderComponent},
+    {path: 'order', loadChildren: './order/order.module#OrderModule'},
     {path: 'order-sumary', component: OrderSumaryComponent},
     {path: 'about', loadChildren: './about/about.module#AboutModule'}
 ];
