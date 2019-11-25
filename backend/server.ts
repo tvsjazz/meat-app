4a43d77022ae7fef3266f8d3964a9a3b1afcf74c
Tiago Siqueira <tiagov@badesc.gov.br>
2019-11-25 12:47:01 -0300

    backend TSlint

--- backend/server.ts
+++ backend/server.ts
@@ -1,26 +1,26 @@
-import * as jsonServer from 'json-server'
-import { Express } from 'express'
+import * as jsonServer from 'json-server';
+import { Express } from 'express';
 
-import * as fs from 'fs'
-import * as https from 'https'
+import * as fs from 'fs';
+import * as https from 'https';
 
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares)
+server.use(middlewares);
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser);
 
 server.post('/login', (req, resp) => {
-  resp.json({message: 'ok'})
-})
+  resp.json({message: 'ok'});
+});
 
 // Use default router
-server.use(router)
+server.use(router);
 
 const options = {
   cert: fs.readFileSync('./backend/keys/cert.pem'),
@@ -29,4 +29,4 @@ const options = {
 
 https.createServer(options, server).listen(3001, () => {
   console.log('JSON Server is running on https://localhost:3001')
-})
+});
